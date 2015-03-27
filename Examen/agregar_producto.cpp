a98a632c61d9a6cf43b703e0925e70dc1145108d
isaac <nirvanox182@gmail.com>
2015-03-27 16:11:52 -0600

    Agregar_Producto

--- Examen/agregar_producto.cpp
+++ Examen/agregar_producto.cpp
@@ -1,14 +1,61 @@
 #include "agregar_producto.h"
 #include "ui_agregar_producto.h"
+#include "cliente.h"
+#include "producto.h"
+#include "productoeducativo.h"
+#include "productoalcohol.h"
+#include "productolujo.h"
+#include <sstream>
+#include <vector>
+#include <string>
 
-agregar_producto::agregar_producto(QWidget *parent) :
+using std::vector;
+using std::string;
+using std::stringstream;
+
+agregar_producto::agregar_producto(QWidget *parent,vector<Producto*>*productos,vector<Cliente*>*clientes,vector<string*>*historial) :
     QDialog(parent),
     ui(new Ui::agregar_producto)
 {
     ui->setupUi(this);
+    this->productos=productos;
+    this->clientes=clientes;
+    this->historial=historial;
 }
 
 agregar_producto::~agregar_producto()
 {
     delete ui;
+}
+
+void agregar_producto::on_Agregar_clicked()
+{
+    //clientes->push_back(new Cliente(ui->ComboBoxCliente->currentText()));
+
+    if(ui->cb_ap_tipo->currentIndex()==0){
+        ProductoEducativo* p_educativo=new ProductoEducativo(ui->SpinBoxVolumen->value(), ui->SpinboxPeso->value(),clientes->at(ui->ComboBoxCliente->currentIndex()), ui->SpinBoxTasa->value());
+        productos.push_back(p_educativo);
+        stringstream ss;
+        ss<< p_educativo->toString()<<" ,"<<p_educativo->CobrarImpuesto();
+        string histo=ss.str();
+        historial->push_back(histo);
+    }
+
+    if(ui->cb_ap_tipo->currentIndex()==1){
+        ProductoAlcohol* p_alcohol = new ProductoEducativo(ui->SpinBoxVolumen->value(), ui->SpinboxPeso->value(),clientes->at(ui->ComboBoxCliente->currentIndex()), ui->SpinBoxTasa->value());
+        productos.push_back(p_alcohol);
+        stringstream ss;
+        ss<< p_educativo->toString()<<" ,"<<p_alcohol->CobrarImpuesto();
+        string histo=ss.str();
+        historial->push_back(histo);
+    }
+
+    if(ui->cb_ap_tipo->currentIndex()==2){
+        Producto* p_lujo = new ProductoLujo(ui->SpinBoxVolumen->value(), ui->SpinboxPeso->value(),clientes->at(ui->ComboBoxCliente->currentIndex()), ui->SpinBoxTasa->value());
+        stringstream ss;
+        ss<< p_educativo->toString()<<" ,"<<p_lujo->CobrarImpuesto();
+        string histo=ss.str();
+        historial->push_back(histo);
+    }
+
 }
--- Examen/agregar_producto.h
+++ Examen/agregar_producto.h
@@ -2,6 +2,16 @@
 #define AGREGAR_PRODUCTO_H
 
 #include <QDialog>
+#include <vector>
+#include <string>
+#include "producto.h"
+#include "productoalcohol.h"
+#include "productoeducativo.h"
+#include "productolujo.h"
+#include "cliente.h"
+
+using std::vector;
+using std::string;
 
 namespace Ui {
 class agregar_producto;
@@ -12,11 +22,17 @@ class agregar_producto : public QDialog
     Q_OBJECT
 
 public:
-    explicit agregar_producto(QWidget *parent = 0);
+    explicit agregar_producto(QWidget *parent = 0, vector<Producto*>* productos=0, vector<Cliente*>* clientes=0, vector<string*>* historial=0);
     ~agregar_producto();
+
+private slots:
+    void on_Agregar_clicked();
 
 private:
     Ui::agregar_producto *ui;
+    vector<Producto*>* productos;
+    vector<Cliente*>* clientes;
+    vector<string*>* historial;
 };
 
 #endif // AGREGAR_PRODUCTO_H
